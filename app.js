df3c82d5c45491f2fb78c4165b11c2ca0601314b
seftel72 <webdev@brad-seftel.com>
2023-11-01 16:57:47 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+http.createServer(function (req,res) {
+res.write('On the way to being a FULL STACK engineer!');
+res.end();
+}
+).listen(3000);
+
+
+console.log('Server Started on port 3K');
+
