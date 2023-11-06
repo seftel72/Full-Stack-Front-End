e3a98391822033e74f952db40e2e65dea8c277c4
Brad Seftel <wevdev@brad-seftel.com>
2023-11-06 14:12:51 -0330

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -12,6 +12,16 @@ const app = express();
 server.on('request', app);
 server.listen(3000, function(){console.log('Sever started on port 3000'); });
 
+process.on("SIGINT", () => {
+    wss.clients.forEach(function each(client){
+        client.close();
+    });
+    server.close(() => {
+        shutDownDB();
+    }    
+  )
+});
+
 //** Begin Websocket */
 const WebSocketServer = require('ws').Server;
 
@@ -26,10 +36,17 @@ wss.on('connection', function connection(ws) {
         console.log('Clients connected currently', numClients);
     }
 
+    db.run(`INSERT INTO visitors (count, time)
+        VALUES (${numClients}, datetime('now'))
+    `);
+
     ws.on('close', function close(){
         wss.broadcast("Current Visitors:" + numClients);
         console.log('Client has disconnected');
     });
+
+        
+
 
     wss.broadcast = function broadcast(data) {
         wss.clients.forEach(function each(client) {
@@ -40,3 +57,33 @@ wss.on('connection', function connection(ws) {
     wss.broadcast("Current Visitors:" + numClients);
 
 });
+
+//** End Websockets */
+
+//** Start DB */
+
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE visitors(
+        count INTEGER,
+        time TEXT
+        )
+    `)
+});
+
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+        console.log(row);
+    })
+}
+
+function shutDownDB(){
+    getCounts();
+    console.log("Shutting Down DB");
+    db.close;
+
+}
